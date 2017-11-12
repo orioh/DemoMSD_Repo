33ddc6882a7e7351db86baae0ca78092f2178982
Cliff Chuang <cliff.chuang@alpha-info.com.tw>
2017-11-12 15:25:30 +0800

    First update 1.simple UI design with Exit function only

---
+++ DemoMSD/DemoMSDDlg.h
@@ -0,0 +1,35 @@
+// DemoMSDDlg.h : header file
+//
+
+#pragma once
+
+
+// CDemoMSDDlg dialog
+class CDemoMSDDlg : public CDialog
+{
+// Construction
+public:
+	CDemoMSDDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_DEMOMSD_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnExit();
+	afx_msg void OnBnClickedBtnInit();
+	afx_msg void OnBnClickedBtnHome();
+	afx_msg void OnBnClickedBtnMove();
+};
